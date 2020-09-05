411078d5af0c58af4d671b46db3e808edb7a474e
camillecay <68919183+camillecay@users.noreply.github.com>
2020-09-05 16:20:12 +0800

    models

---
+++ job.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+var Schema = mongoose.Schema
+
+const jobSchema= new Schema({ 
+  jobTitle:String,
+  jobCreator: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+  jobDesc:String,
+  jobCategory: String,
+  jobDuration:String,
+  jobSkills:[String],
+  jobStatus:String,
+  jobApplicants:[{type: mongoose.Schema.Types.ObjectId, ref: 'user'}],
+  postDate: Date,
+  chargeRate:Number,
+  approvedUser: {type: mongoose.Schema.Types.ObjectId, ref: 'user'}
+})
+
+const jobModel = mongoose.model('job', jobSchema)
+
+module.exports = {
+  jobModel
+}
+   
