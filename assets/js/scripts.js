b573f14d7a9c351bad9fb2ae13ef054ac4ba1b62
mcxstillo <68911214+mcxstillo@users.noreply.github.com>
2020-09-28 23:29:08 +0800

    Added date to job cards

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
     })
 
     $("button[name='ratingbtn']").click(function() {
-
+        console.log(rating);
         $.ajax({
             type:"POST",
             url:"/rating",
